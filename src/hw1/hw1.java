5ea6d12a73c8a9986756365854657077d5b3a14f
user <user@EA06>
2020-09-29 09:39:47 +0800

    hw1

--- src/hw1/hw1.java
+++ src/hw1/hw1.java
@@ -3,7 +3,7 @@ package hw1;
 public class hw1 {
 
 	public static void main(String[] args) {
-	System.out.println("I'm ´^­³ÐÌ");
+	System.out.println("I'm Benson");
 	System.out.println("My nid is D0843718");
 	System.out.println("This is lab 1 hw");
 	}
